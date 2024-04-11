7859d20acdfdb5ad7b5643531c32bf94c552b008
DevTechBytes <devvy@DevTechBytes.com>
2024-04-10 20:50:19 -0500

    initial code check-in

--- config.py
+++ config.py
@@ -4,8 +4,3 @@ https://www.youtube.com/@DevTechBytes
 """
 class Config:
     PAGE_TITLE = "Ollama Langchain Chatbot"
-
-    SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
-                    open-source models.
-                    You can can answer questions for users on any topic."""
-    
